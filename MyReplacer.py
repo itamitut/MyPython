9be83308a7f005865e73332e253263163145a4b8
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-09-24 11:39:31 +0300

    24.09

--- MyReplacer.py
+++ MyReplacer.py
@@ -1,5 +1,9 @@
+import os
 import re
-with open('a.txt', 'r') as a, open('a1.txt', 'w') as a1:
-    for line in a:
-        line = re.sub(r'(#.*?#)', 'Новый шаблон', line)
-        a1.write(line)
+for filename in os.listdir("."):
+    if filename.endswith(".template"):
+        outfilename = re.sub(r".template", "", filename)
+        with open(filename, 'r') as a, open(outfilename, 'w') as a1:
+            for line in a:
+                line = re.sub(r'(#.*?#)', 'Новый шаблон', line)
+                a1.write(line)
