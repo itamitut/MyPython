2fb6dccd756e2cacad5e2428c9bbdbf9ed6c4a83
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-09-26 16:44:06 +0300

    selenium

--- DateTime.py
+++ DateTime.py
@@ -1,6 +1,13 @@
-from datetime import date
-
+from datetime import datetime
+import re
+#Создаем файл и записываем в него текущую дату:
 f = open('today.txt', 'w')
-this_day = date.today()
-f.write(str(this_day))
+now = datetime.now()
+f.write(str(now))
 f.close()
+# Cчитываем строку и ищем в ней соответствие по шаблону:
+with open('today.txt', 'r') as f:
+    today_string = f.readline()
+match = re.findall(r'-(.*)-', today_string)
+print(match)
+
